87f7ba8865d1911eaa786cbddfdcc55d24f910cb
samandaribragimov <samandar0406@gmail.com>
2023-01-12 16:19:56 +0100

    Done

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,6 +2,18 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        //Declaring variables
+        int first;
+        int second;
+        int third;
+
+        //Assigning values
+        first = 1;
+        second = 10;
+        third = 100;
+
+        //print to console
+        System.out.println(first + "\n" + second + "\n" + third);
+
     }
 }
